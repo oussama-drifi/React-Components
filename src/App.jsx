95b962e694d22f5760a73c9c4441af9fc6c0f161
oussamadrf <oussamadrifi107@gmail.com>
2025-10-29 22:24:02 +0100

    displayed accordion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react'
 import '@fontsource/poppins';
 
+import Accordion from './components/accordion/accordion'
+
 function App() {
+
+  const questions = [
+    {
+      question: "what time is it", 
+      answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem incidunt culpa sunt excepturi voluptatibus vitae quod quas reprehenderit porro minima eos odit quo tenetur cupiditate."
+    }, {
+      question: "what programming languages do you like",
+      answer: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima perferendis, iste molestias ea corrupti provident minus aut est dignissimos ad. Magnam architecto ullam aliquam quidem."
+    }, {
+      question: "for how long have you been coding",
+      answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi fugiat quos odio, eveniet nostrum harum illo, mollitia blanditiis dolores possimus magnam minus necessitatibus. Rerum et quia corporis nemo alias corrupti."
+    }
+  ]
 
   return (
     <>
-      <h1>hello world</h1>
+      <Accordion questions={questions}/>
     </>
   )
 }
