52984b0e66abeb3116046042054b4b23a7618c19
oussamadrf <oussamadrifi107@gmail.com>
2025-11-02 17:39:43 +0100

    added password component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
 import '@fontsource/poppins';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 import Accordion from './components/accordion/accordion'
+import PasswordStrengthChecker from './components/passwordStrengthChecker'
 
 function App() {
 
   const questions = [
     {
-      question: "what time is it", 
+      question: "are you working on a some project?",
       answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem incidunt culpa sunt excepturi voluptatibus vitae quod quas reprehenderit porro minima eos odit quo tenetur cupiditate."
     }, {
-      question: "what programming languages do you like",
+      question: "what programming languages do you like?",
       answer: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima perferendis, iste molestias ea corrupti provident minus aut est dignissimos ad. Magnam architecto ullam aliquam quidem."
     }, {
-      question: "for how long have you been coding",
+      question: "for how long have you been coding?",
       answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi fugiat quos odio, eveniet nostrum harum illo, mollitia blanditiis dolores possimus magnam minus necessitatibus. Rerum et quia corporis nemo alias corrupti."
     }
   ]
@@ -22,6 +22,7 @@ function App() {
   return (
     <>
       <Accordion questions={questions}/>
+      <PasswordStrengthChecker />
     </>
   )
 }
--- src/components/passwordStrengthChecker/passwordStrengthChecker.jsx
+++ src/components/passwordStrengthChecker/passwordStrengthChecker.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import './passwordStrengthChecker.css';
-import { useEffect } from 'react';
 
 const PasswordStrengthChecker = () => {
 
