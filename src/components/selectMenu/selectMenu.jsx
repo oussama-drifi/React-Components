d738af7fefd042a74db5bbf3be5021ec880c6ba0
oussamadrf <oussamadrifi107@gmail.com>
2025-11-18 23:57:58 +0100

    done

--- src/components/selectMenu/selectMenu.jsx
+++ src/components/selectMenu/selectMenu.jsx
@@ -1,15 +1,27 @@
+import { useState } from 'react'
 import './selectMenu.css'
 
 const SelectMenu = ({options}) => {
+    const [isShown, setIsShown] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(options[0]);
+    
+    const handleChange = (option) => {
+        setSelectedOption(option);
+        setIsShown(false);
+    }
     return (
         <div className="select-wrapper">
             <input type="hidden" value="" id="selected-option" />
             <div className="selected-option">
-                <span className="selected">{options[0]}</span> <span><i className="bi bi-chevron-down"></i></span>
+                <span className="selected">{selectedOption}</span> <span><i className="bi bi-chevron-down"></i></span>
             </div>
-            <ul className="options">
+            <ul className={`options ${isShown ? "options-shown" : ""}`}>
                 {
-                    options.map(option => (<li key={option}>{option}</li>))
+                    options.map(option => (
+                        <li key={option}
+                            onClick={() => handleChange(option)}
+                        >{option}</li>
+                    ))
                 }
             </ul>
         </div>
