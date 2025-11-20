9ca4eace86467ba44bbea7099dce36da60500ac8
oussamadrf <oussamadrifi107@gmail.com>
2025-11-20 20:25:58 +0100

    done with custom menu

--- src/components/selectMenu/selectMenu.jsx
+++ src/components/selectMenu/selectMenu.jsx
@@ -1,19 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './selectMenu.css'
 
 const SelectMenu = ({options}) => {
     const [isShown, setIsShown] = useState(false);
     const [selectedOption, setSelectedOption] = useState(options[0]);
-    
+
     const handleChange = (option) => {
         setSelectedOption(option);
-        setIsShown(false);
+        setIsShown(prev => !prev);
     }
+
+    const selectedOptionRef = useRef(null);
+    
+    useEffect(() => {
+        const closeMenuOnOuterClick = (e) => {
+            if(!selectedOptionRef.current.contains(e.target)){
+                setIsShown(false);
+            }
+        }
+        document.addEventListener('click', closeMenuOnOuterClick)
+        return () => {
+            document.removeEventListener('click', closeMenuOnOuterClick)
+        }
+    }, [])
+
     return (
         <div className="select-wrapper">
-            <input type="hidden" value="" id="selected-option" />
-            <div className="selected-option">
-                <span className="selected">{selectedOption}</span> <span><i className="bi bi-chevron-down"></i></span>
+            <input 
+                type="hidden" 
+                value={selectedOption}
+                id="selected-option"/>
+            <div className="selected-option" onClick={() => handleChange(selectedOption)} ref={selectedOptionRef}>
+                <span className="selected">{selectedOption}</span> <span className={isShown ? "rotate" : ""}><i className={`bi bi-chevron-down`}></i></span>
             </div>
             <ul className={`options ${isShown ? "options-shown" : ""}`}>
                 {
